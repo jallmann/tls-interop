b04f904c176569c8b39ce8398fbc86784ac138ae
Jonas Allmann <jallmann@mozilla.com>
2018-06-26 11:14:55 +0200

    Clean up travis-script.sh

--- travis_script.sh
+++ travis_script.sh
@@ -1,52 +1,28 @@
 #!/bin/bash
 
-#apt-get update
-
-#apt-get install git -y
-#apt-get install cmake -y
-#sudo apt-get install build-essential -y
-#sudo apt-get install golang -y
-#apt-get install mercurial -y
-#sudo apt-get install gyp -y
-#sudo apt-get install ninja-build -y
-#apt-get install clang -y
-#sudo apt-get install g++-6 -y
-#sudo apt-get install g++-4.8 -y
-#sudo apt-get install zlib1g-dev -y
-#apt-get install curl -y
-
-#apt-get autoremove -y
-#apt-get clean
-#apt-get autoclean
-#sudo apt-get install gyp -y
-
 sudo apt-get -qq update
 sudo apt-get install ninja-build -y
 sudo apt-get install zlib1g-dev -y
 
-cd ..
+ROOT_DIR=$TRAVIS_BUILD_DIR/..
+
+cd $ROOT_DIR
 git clone https://chromium.googlesource.com/external/gyp
 cd gyp
 ./setup.py build
 sudo ./setup.py install
-cd ..
+cd $ROOT_DIR
 hg clone https://hg.mozilla.org/projects/nspr
 hg clone https://hg.mozilla.org/projects/nss
 cd nss
 ./build.sh
-cd ..
+cd $ROOT_DIR
 git clone https://github.com/google/boringssl.git
 cd boringssl
 mkdir build
 cd build
 cmake ..
 make
-cd ../../tls-interop/
-
-#cargo build
-#echo $HOST
-#echo $DOMSUF
-#cat /etc/hosts
-#cargo run -- --client /home/travis/build/jallmann/dist/Debug/bin/nss_bogo_shim --server /home/travis/build/jallmann/boringssl/build/ssl/test/bssl_shim --rootdir /home/travis/build/jallmann/boringssl/ssl/test/runner/ --test-cases cases.json --client-writes-first
+cd $TRAVIS_BUILD_DIR
 
 cargo test
